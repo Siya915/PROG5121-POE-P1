a7ce722e995674d68e819ef6be8569fd71cda9ef
Siya915 <st10495241@rcconnect.edu.za>
2026-04-13 12:42:22 +0200

    Final POE
    All the final code

---
+++ Siyabonga_PROG_P1/src/loginapp/Login.java
@@ -0,0 +1,65 @@
+package loginapp;
+
+import java.util.regex.Pattern;
+
+/**
+ * Cell phone regex adapted from:
+ * https://stackoverflow.com/questions/42179046/java-regex-for-phone-number-with-country-code
+ */
+
+public class Login {
+
+    private String storedUsername;
+    private String storedPassword;
+    private String firstName;
+    private String lastName;
+
+    public boolean checkUserName(String username) {
+        return username.contains("_") && username.length() <= 5;
+    }
+
+    public boolean checkPasswordComplexity(String password) {
+        String regex = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9]).{8,}$";
+        return Pattern.matches(regex, password);
+    }
+
+    public boolean checkCellPhoneNumber(String cellPhone) {
+        String regex = "^\\+27[0-9]{9}$";
+        return Pattern.matches(regex, cellPhone);
+    }
+
+    public String registerUser(String username, String password, String cellPhone,
+                               String firstName, String lastName) {
+
+        if (!checkUserName(username)) {
+            return "Username is not correctly formatted; please ensure that your username contains an underscore and is no more than five characters in length.";
+        }
+
+        if (!checkPasswordComplexity(password)) {
+            return "Password is not correctly formatted; please ensure that the password contains at least eight characters, a capital letter, a number, and a special character.";
+        }
+
+        if (!checkCellPhoneNumber(cellPhone)) {
+            return "Cell phone number incorrectly formatted or does not contain international code.";
+        }
+
+        this.storedUsername = username;
+        this.storedPassword = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+
+        return "User registered successfully.";
+    }
+
+    public boolean loginUser(String username, String password) {
+        return username.equals(storedUsername) && password.equals(storedPassword);
+    }
+
+    public String returnLoginStatus(boolean loginStatus) {
+        if (loginStatus) {
+            return "Welcome " + firstName + ", " + lastName + " it is great to see you again.";
+        } else {
+            return "Username or password incorrect, please try again.";
+        }
+    }
+}
---
+++ Siyabonga_PROG_P1/src/loginapp/MainApp.java
@@ -0,0 +1,43 @@
+package loginapp;
+
+import java.util.Scanner;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        Login login = new Login();
+
+        System.out.println("=== REGISTER ===");
+
+        System.out.print("Enter first name: ");
+        String firstName = input.nextLine();
+
+        System.out.print("Enter last name: ");
+        String lastName = input.nextLine();
+
+        System.out.print("Enter username: ");
+        String username = input.nextLine();
+
+        System.out.print("Enter password: ");
+        String password = input.nextLine();
+
+        System.out.print("Enter SA cell number (+27...): ");
+        String cell = input.nextLine();
+
+        String regMessage = login.registerUser(username, password, cell, firstName, lastName);
+        System.out.println(regMessage);
+
+        System.out.println("\n=== LOGIN ===");
+
+        System.out.print("Enter username: ");
+        String loginUser = input.nextLine();
+
+        System.out.print("Enter password: ");
+        String loginPass = input.nextLine();
+
+        boolean status = login.loginUser(loginUser, loginPass);
+        System.out.println(login.returnLoginStatus(status));
+    }
+}
